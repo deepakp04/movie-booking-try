96ac92a1b3521ce7bad8a37a73c0c28be860886b
Deepak <deepakdpk274@gmail.com>
2026-09-03 10:56:19 +0530

    analytics - bugs

--- src/main/java/com/moviebooking/analytics/repository/AnalyticsRepository.java
+++ src/main/java/com/moviebooking/analytics/repository/AnalyticsRepository.java
@@ -758,7 +758,7 @@ public class AnalyticsRepository {
             JOIN theatres t ON scr.theatre_id = t.id
             WHERE m.is_deleted = false AND s.is_deleted = false
               AND scr.is_deleted = false AND t.is_deleted = false
-              AND s.start_time < NOW()
+              
               """ + theatreFilter + """
             ORDER BY m.title
             """;
@@ -774,7 +774,7 @@ public class AnalyticsRepository {
             JOIN screens scr ON scr.theatre_id = t.id
             JOIN shows s ON s.screen_id = scr.id
             WHERE t.is_deleted = false AND scr.is_deleted = false AND s.is_deleted = false
-              AND s.start_time < NOW()
+              
               """ + theatreFilter + """
             ORDER BY t.name
             """;
@@ -790,7 +790,7 @@ public class AnalyticsRepository {
             JOIN theatres t ON scr.theatre_id = t.id
             JOIN shows s ON s.screen_id = scr.id
             WHERE scr.is_deleted = false AND t.is_deleted = false AND s.is_deleted = false
-              AND s.start_time < NOW()
+              
               """ + theatreFilter + """
             ORDER BY scr.name
             """;
@@ -808,7 +808,7 @@ public class AnalyticsRepository {
             JOIN shows s ON s.screen_id = scr.id
             WHERE c.is_deleted = false AND t.is_deleted = false
               AND scr.is_deleted = false AND s.is_deleted = false
-              AND s.start_time < NOW()
+              
               """ + (restrictToTheatreId != null
                   ? " AND t.id = " + restrictToTheatreId
                   : "") + """
